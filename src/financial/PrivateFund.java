2b5046b227d9f522ef5baaa29917c274a755b4bc
Jiachang Xu <jiachanx@usc.edu>
2017-07-12 10:31:09 +0800

    Complete PrivateFund Class

---
+++ src/financial/PrivateFund.java
@@ -0,0 +1,10 @@
+package financial;
+
+public class PrivateFund extends InstrumentFinancial{
+
+	public PrivateFund(String inCode, String inName, double inEAR, double inTotal) {
+		super(inCode, inName, inEAR, inTotal, 1.00);
+		// TODO Auto-generated constructor stub
+	}
+
+}
