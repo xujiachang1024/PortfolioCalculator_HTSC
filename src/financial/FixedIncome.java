c03211ad3f533b2728ae1596624a197d37d95bc7
Jiachang Xu <jiachanx@usc.edu>
2017-07-12 10:29:18 +0800

    Complete FixedIncome Class

---
+++ src/financial/FixedIncome.java
@@ -0,0 +1,10 @@
+package financial;
+
+public class FixedIncome extends InstrumentFinancial{
+
+	public FixedIncome(String inCode, String inName, double inEAR, double inTotal) {
+		super(inCode, inName, inEAR, inTotal, 0.00);
+		// TODO Auto-generated constructor stub
+	}
+
+}
